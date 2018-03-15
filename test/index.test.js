2d0d04376c0725d3db8fccce3c3e51657fd8d9d1
Thompson, Brett <brett.thompson@tsi.com>
2018-03-15 13:01:45 -0500

    Fixed bug with false end marker

--- test/index.test.js
+++ test/index.test.js
@@ -17,11 +17,11 @@ beforeEach(function() {
 });
 
 describe("_parse", function() {
-    it("Should fail to parse and not emit", function(done) {
+    it("Should fail to parse", function(done) {
         parser.on("json", (json) => {
             done("Should not have parsed");
         });
-        parser._parse("asdf");
+        expect(parser._parse("asdf")).to.be.instanceof(SyntaxError);
         done();
     });
 
@@ -29,8 +29,7 @@ describe("_parse", function() {
         parser.on("json", (json) => {
             done();
         });
-        parser._parse("asdf");
-        done();
+        expect(parser._parse("{}")).to.be.undefined;
     })
 });
 
@@ -264,6 +263,50 @@ describe('_algorithm1', function() {
         });
     });
 
+    it("Should parse one message split between two packets", function (done) {
+        parser._buffer = Buffer.from(`{"test":"te`, 'utf8');
+
+        parser._algorithm1(() => {
+            expect(parser._buffer).to.be.instanceof(Buffer);
+            expect(parser._buffer).to.deep.equal(Buffer.from(`{"test":"te`, 'utf8'));
+            expect(obj).to.have.lengthOf(0);
+            
+            parser._buffer = Buffer.concat([
+                Buffer.from(`{"test":"te`, 'utf8'),
+                Buffer.from(`st"}`, 'utf8')
+            ])
+            parser._algorithm1(() => {
+                expect(parser._buffer).to.be.instanceof(Buffer);
+                expect(parser._buffer.length).length.to.equal(0);
+                expect(obj).to.have.lengthOf(1);
+                expect(obj[0]).to.deep.eq({test:"test"});
+                done();
+            });
+        });
+    });
+
+    it("Should parse two packets, one ending with false end", function (done) {
+        parser._buffer = Buffer.from(`{"test":{}`, 'utf8');
+
+        parser._algorithm1(() => {
+            expect(parser._buffer).to.be.instanceof(Buffer);
+            expect(parser._buffer).to.deep.equal(Buffer.from(`{"test":{}`, 'utf8'));
+            expect(obj).to.have.lengthOf(0);
+            
+            parser._buffer = Buffer.concat([
+                Buffer.from(`{"test":{}`, 'utf8'),
+                Buffer.from(`}`, 'utf8')
+            ])
+            parser._algorithm1(() => {
+                expect(parser._buffer).to.be.instanceof(Buffer);
+                expect(parser._buffer.length).length.to.equal(0);
+                expect(obj).to.have.lengthOf(1);
+                expect(obj[0]).to.deep.eq({test:{}});
+                done();
+            });
+        });
+    });
+
     it("Should parse two messages partially split between two packets", function(done) {
         parser._buffer = Buffer.from(`{"test":"te`, 'utf8');
 
@@ -360,6 +403,28 @@ describe('_algorithm2', function() {
         });
     });
 
+    it("Should parse one message split between two packets", function (done) {
+        parser._buffer = Buffer.from(`{"test":"te`, 'utf8');
+
+        parser._algorithm2(() => {
+            expect(parser._buffer).to.be.instanceof(Buffer);
+            expect(parser._buffer).to.deep.equal(Buffer.from(`{"test":"te`, 'utf8'));
+            expect(obj).to.have.lengthOf(0);
+            
+            parser._buffer = Buffer.concat([
+                Buffer.from(`{"test":"te`, 'utf8'),
+                Buffer.from(`st"}`, 'utf8')
+            ])
+            parser._algorithm2(() => {
+                expect(parser._buffer).to.be.instanceof(Buffer);
+                expect(parser._buffer.length).length.to.equal(0);
+                expect(obj).to.have.lengthOf(1);
+                expect(obj[0]).to.deep.eq({test:"test"});
+                done();
+            });
+        });
+    });
+
     it("Should parse two messages partially split between two packets", function(done) {
         parser._buffer = Buffer.from(`{"test":"te`, 'utf8');
 
@@ -453,6 +518,28 @@ describe('_algorithm3', function() {
             expect(obj).to.have.lengthOf(1);
             expect(obj[0]).to.deep.eq({});
             done();
+        });
+    });
+
+    it("Should parse one message split between two packets", function (done) {
+        parser._buffer = Buffer.from(`${BEGIN_MARKER}{"test":"te`, 'utf8');
+
+        parser._algorithm3(() => {
+            expect(parser._buffer).to.be.instanceof(Buffer);
+            expect(parser._buffer).to.deep.equal(Buffer.from(`${BEGIN_MARKER}{"test":"te`, 'utf8'));
+            expect(obj).to.have.lengthOf(0);
+            
+            parser._buffer = Buffer.concat([
+                Buffer.from(`${BEGIN_MARKER}{"test":"te`, 'utf8'),
+                Buffer.from(`st"}${END_MARKER}`, 'utf8')
+            ])
+            parser._algorithm3(() => {
+                expect(parser._buffer).to.be.instanceof(Buffer);
+                expect(parser._buffer.length).length.to.equal(0);
+                expect(obj).to.have.lengthOf(1);
+                expect(obj[0]).to.deep.eq({test:"test"});
+                done();
+            });
         });
     });
 
